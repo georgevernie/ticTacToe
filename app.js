1c8922a57bcca9a7be1509035d2c0452d90526b0
Jonathan Langner <jonlangner@gmail.com>
2020-02-25 09:24:41 -0600

    Changed logic comparisons to be more strongly typed.

--- app.js
+++ app.js
@@ -14,32 +14,32 @@ cell.forEach(function (cell) {
 
 function cellClicked(e) {
     // * Basic Movement And Turn Logic
-    if (player == 0 && e.target.textContent != 'X' && e.target.textContent != 'O') {
+    if (player === 0 && e.target.textContent !== 'X' && e.target.textContent !== 'O') {
         e.target.textContent = 'X';
         player++;
         moveCount++;
     }
-    else if (player == 1 && e.target.textContent != 'X' && e.target.textContent != 'O') {
+    else if (player === 1 && e.target.textContent !== 'X' && e.target.textContent !== 'O') {
         e.target.textContent = 'O';
         player--;
         moveCount++;
     }
     //* Lets the players know that there is something in that cell already.
-    else if (e.target.textContent == 'X' || e.target.textContent == 'O') {
+    else if (e.target.textContent === 'X' || e.target.textContent === 'O') {
         alert("Invalid Move!");
     }
 
     // * Checks each horizontal section to see if there was a win.
     for(let x = 0; x <= 6; x += 3){
-        if (cell[x].textContent == 'O' || cell[x].textContent == 'X') {
-            if (cell[x + 1].textContent == cell[x].textContent && cell[x + 2].textContent == cell[x].textContent) {
+        if (cell[x].textContent === 'O' || cell[x].textContent === 'X') {
+            if (cell[x + 1].textContent === cell[x].textContent && cell[x + 2].textContent === cell[x].textContent) {
                 alert(cell[x].textContent + "'s Won Horizontally!");
                 isDone = true;
-                if(cell[x].textContent == 'X'){
+                if(cell[x].textContent === 'X'){
                     player1Score++;
                     scoreBoard[0].textContent = player1Score;
                 }
-                else if(cell[x].textContent == 'O'){
+                else if(cell[x].textContent === 'O'){
                     player2Score++;
                     scoreBoard[1].textContent = player2Score;
                 }
@@ -48,34 +48,34 @@ function cellClicked(e) {
     }
     //* Checks each row vertically for wins
     for(let x = 0; x <= 2; x++){
-        if (cell[x].textContent == 'O' || cell[x].textContent == 'X') {
-            if (cell[x + 3].textContent == cell[x].textContent && cell[x + 6].textContent == cell[x].textContent) {
+        if (cell[x].textContent === 'O' || cell[x].textContent === 'X') {
+            if (cell[x + 3].textContent === cell[x].textContent && cell[x + 6].textContent === cell[x].textContent) {
                 alert(cell[x].textContent + "'s Won Vertically!");
                 isDone = true;
-                if(cell[x].textContent == 'X'){
+                if(cell[x].textContent === 'X'){
                     player1Score++;
                     scoreBoard[0].textContent = player1Score;
                 }
-                else if(cell[x].textContent == 'O'){
+                else if(cell[x].textContent === 'O'){
                     player2Score++;
                     scoreBoard[1].textContent = player2Score;
                 }
             }
         }
     }
-    // * Checks the Diagonals for a win.
-    
+
+    //* Checks the Diagonals for a win.
     x = 0;
     //Diagonal 1
-    if (x == 0 && cell[x].textContent == 'O' || cell[x].textContent == 'X') {
-        if (cell[x + 4].textContent == cell[x].textContent && cell[x + 8].textContent == cell[x].textContent) {
+    if (x === 0 && cell[x].textContent === 'O' || cell[x].textContent === 'X') {
+        if (cell[x + 4].textContent === cell[x].textContent && cell[x + 8].textContent === cell[x].textContent) {
             alert(cell[x].textContent + "'s Won Diagonally!");
             isDone = true;
-            if(cell[x].textContent == 'X'){
+            if(cell[x].textContent === 'X'){
                 player1Score++;
                 scoreBoard[0].textContent = player1Score;
             }
-            else if(cell[x].textContent == 'O'){
+            else if(cell[x].textContent === 'O'){
                 player2Score++;
                 scoreBoard[1].textContent = player2Score;
             }
@@ -84,22 +84,22 @@ function cellClicked(e) {
     
     x = 2;
     //Diagonal 2
-    if (x == 2 && cell[x].textContent == 'O' || cell[x].textContent == 'X') {
-        if (cell[x + 2].textContent == cell[x].textContent && cell[x + 4].textContent == cell[x].textContent) {
+    if (x === 2 && cell[x].textContent === 'O' || cell[x].textContent === 'X') {
+        if (cell[x + 2].textContent === cell[x].textContent && cell[x + 4].textContent === cell[x].textContent) {
             alert(cell[x].textContent + "'s Won Diagonally!");
             isDone = true;
-            if(cell[x].textContent == 'X'){
+            if(cell[x].textContent === 'X'){
                 player1Score++;
                 scoreBoard[0].textContent = player1Score;
             }
-            else if(cell[x].textContent == 'O'){
+            else if(cell[x].textContent === 'O'){
                 player2Score++;
                 scoreBoard[1].textContent = player2Score;
             }
         }
     }
     //* Checks for a tied Game.
-    if (moveCount == 9 && isDone == false) {
+    if (moveCount === 9 && isDone === false) {
         alert("Game Ended with a Tie!");
         isDone = true;
         player1Score++;
@@ -109,7 +109,7 @@ function cellClicked(e) {
 
     }
     //* Clears the Game board and sets values for the next game.
-    if (isDone == true){
+    if (isDone === true){
         for(let z = 0; z < cell.length; z++){
             cell[z].textContent = '';
         }
@@ -117,6 +117,4 @@ function cellClicked(e) {
         moveCount = 0;
         player = 0;
     }
-    
-    
 }
